850172025a16bab6229d14670452bc03e4e1bfb0
jo <jotech@users.noreply.github.com>
2017-12-06 18:40:43 +0100

    addReactByString: fixed issue for exchange reactions
    - special case when only one metabolite is given: "pyr[e] <=> "
      -> take care of empty product string

--- R/addReactByString.R
+++ R/addReactByString.R
@@ -32,7 +32,7 @@ addReactByString <- function(model,
   Scoef <- list()
   rev   <- vector(mode="logical", length=Nids)
   for( i in 1:Nids){
-    str    <- react_str[i]
+    str    <- str_strip(react_str[i])
     dirtmp <- list("<==>"="BOTH", "<-->"="BOTH", "<=>"="BOTH", "<->"="BOTH",
                    "==>"="FORW", "-->"="FORW", "=>"="FORW", "->"="FORW",
                    "<=="="BACK", "<--"="BACK", "<="="BACK", "<-"="BACK") # TODO detection may cause problem, order of list is important (e.g. <== before <=)
@@ -46,8 +46,8 @@ addReactByString <- function(model,
     edu     <- get_reactants(edu_str)
     pro     <- get_reactants(pro_str)
     
-    Scoef    <- c(Scoef, list(c(-1 * as.numeric(edu[2,]), as.numeric(pro[2,]))) )
-    mets     <- c(mets,  list(unname(c(edu[1,], pro[1,]))) )
+    Scoef    <- c(Scoef, list(rm.na(c(-1 * as.numeric(edu[2,]), as.numeric(pro[2,])))) )
+    mets     <- c(mets,  list(rm.na(unname(c(edu[1,], pro[1,])))) )
   }
   
   lb <- ifelse(rev==TRUE, -1000, 0) # always the case?
@@ -58,6 +58,7 @@ addReactByString <- function(model,
 }
 
 get_reactants <- function(str){
+  if(str=="") return(matrix(c(NA,NA), nrow=2))
   split <- unlist(str_split(str, "\\+"))
   stoich<- sapply(split, function(s){
     s <- str_strip(s)
@@ -76,4 +77,10 @@ get_reactants <- function(str){
 
 str_strip <- function(str){
   return(gsub("^\\s+","",gsub("\\s+$","",str)))
+}
+
+rm.na <- function(vec){
+  idx <- which(is.na(vec))
+  if(length(idx)>0) return(vec[-idx])
+  else return(vec)
 }
